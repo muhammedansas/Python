d16d7e4e8817272c3008fa300c8eac7936738d5a
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-03-16 22:02:16 +0530

    updated

--- inheritence.py
+++ inheritence.py
@@ -16,20 +16,42 @@
 # //////////////////////////////////////////
 # multiple inheritence:
 # ///////////////////////////////////////////
-class Main:
-    def first(self):
-        print("its first method")
+# class Main:
+#     def first(self):
+#         print("its first method")
 
-class Second:
-    def second(self):
-        print("its second method")
+# class Second:
+#     def second(self):
+#         print("its second method")
 
-class Child(Main,Second):
-    def third(self):
-        print("its third method")
+# class Child(Main,Second):
+#     def third(self):
+#         print("its third method")
     
 
-obj1 = Child() 
-obj1.first()
-obj1.second()
-obj1.third()     
+# obj1 = Child() 
+# obj1.first()
+# obj1.second
+# obj1.third() 
+
+# /////////////////////////////////////////
+# multilevel inheritence:
+# ////////////////////////////////////////
+# class Main:
+#     def first(self):
+#         print("its first method")
+
+# class Child1(Main):
+#     def second(self):
+#         print("its second method")
+
+# class Chaild2(Child1):
+#     def third(self):
+#         print("its third method")
+
+# obj1 = Chaild2()
+# obj1.first()    
+# obj1.second()
+# obj1.third()    
+
+# /////////////////////////////////////////////////
