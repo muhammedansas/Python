d9d8d24c14f2c3cb67563bf5c9487cede05332a6
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-03-16 21:57:06 +0530

    updated

--- inheritence.py
+++ inheritence.py
@@ -1,12 +1,35 @@
-class School:
-    def make_sound(self):
-        print("this is an lp section")
+# single inheritnce:
+# //////////////////////////////////////////
+# class School:
+#     def make_sound(self):
+#         print("this is an lp section")
 
-class Student(School):
-    def make_sound(self):
-       print("hi")
+# class Student(School):
+#     def make_sound(self):
+#        print("hi")
        
         
 
-s1 = Student()
-s1.make_sound()
+# s1 = Student()
+# s1.make_sound()
+
+# //////////////////////////////////////////
+# multiple inheritence:
+# ///////////////////////////////////////////
+class Main:
+    def first(self):
+        print("its first method")
+
+class Second:
+    def second(self):
+        print("its second method")
+
+class Child(Main,Second):
+    def third(self):
+        print("its third method")
+    
+
+obj1 = Child() 
+obj1.first()
+obj1.second()
+obj1.third()     
