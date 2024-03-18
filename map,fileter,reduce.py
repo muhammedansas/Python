0d398d1f28c6e7632b24c302b82571d8b3097674
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-03-18 23:11:56 +0530

    updated

--- map,fileter,reduce.py
+++ map,fileter,reduce.py
@@ -63,6 +63,6 @@ from functools import reduce
 
 # //////////////////////////////////////////////////////////////////
 
-a= 10
-b=20
-print("entered number is {} fghjk{}".format(a,b))
+# a= 10
+# b=20
+# print("entered number is {} fghjk{}".format(a,b))
