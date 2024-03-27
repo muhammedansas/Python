217a0cd6132e168b9b0e0b8fc2168c950b4fe544
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-03-27 10:52:14 +0530

    updated

---
+++ Encapsulation.py
@@ -0,0 +1,19 @@
+# encapsulation:
+# //////////////////////////////
+# private attribute and method:
+# ///////////////////////////////
+
+# class encaps:
+#     def __init__(self):
+#         self.__name = "ansas"
+
+#     def __main(self):
+#         print(self.__name)    
+
+# obj = encaps()                        
+# print(obj._encaps__name)                # : this is name angling for access a private attribute
+
+# obj._encaps__main()                     # : this is name angling for access a private method
+        
+# /////////////////////////////////////////////////////////////////////////////////////////////         
+
