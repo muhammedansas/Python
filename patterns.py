927d4460e84a106c484313cb432cc5de89e28fe6
Muhammed Ansas pt <muhammedansas566@gmail.com>
2025-01-13 15:37:05 +0530

    updated

--- patterns.py
+++ patterns.py
@@ -1,6 +1,38 @@
-num = 3
-for i in range(1,num+1):
-    for j in range(1,i+1):
-        print("*",end=" ")
+# //////////////// Print a Right-Angled Triangle of Stars \\\\\\\\\\\\\\\\
 
-    print()
+# rows = 5
+# for i in range(1, rows + 1):
+#     print("*" * i)
+
+
+# ////////////// Print an Inverted Right-Angled Triangle of Stars \\\\\\\\\\\\\\
+
+# rows = 5
+# for i in range(rows, 0, -1):
+#     print("*" * i)
+
+
+# /////////////// Print a Pyramid Pattern \\\\\\\\\\\\\\
+
+# rows = 5
+# for i in range(1, rows + 1):
+#     spaces = rows - i
+#     stars = 2 * i - 1
+#     print(" " * spaces + "*" * stars)
+
+
+# //////////// Print a Diamond Pattern \\\\\\\\\\\\\\
+
+# rows = 5
+
+# # Upper part
+# for i in range(1, rows + 1):
+#     spaces = rows - i
+#     stars = 2 * i - 1
+#     print(" " * spaces + "*" * stars)
+
+# # Lower part
+# for i in range(rows - 1, 0, -1):
+#     spaces = rows - i
+#     stars = 2 * i - 1
+#     print(" " * spaces + "*" * stars)
